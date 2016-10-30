d887b1ac509011d8fb861118e6bcaa4219143470
Tony DiCola <tony@tonydicola.com>
2016-10-30 14:27:34 -0700

    Minor version bump for new release.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name='micropython-adafruit-pca9685',
     py_modules=['pca9685', 'servo', 'motor', 'stepper'],
-    version="1.1",
+    version="1.1.1",
     description="Driver for MicroPython for the PCA9685 PWM driver.",
     long_description="""\
 This library lets you control the motor, stepper, and servo drivers based on PCA9685.""",
